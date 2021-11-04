76de484c84b674fcc80ba726b3e5264b4623d025
Carlos Henrique Rodrigues da Silva <carlos.silva@pampa.compasso>
2021-11-04 19:01:24 -0300

    MVP frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,28 +1,24 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import GlobalStateContext from "../../global/GlobalStateContext";
 import useForm from "../../hooks/useForm";
-import { createPartner } from "../../services/createPartner";
+import { createPartner } from "../../services/partner";
 import { Button, Input, Main } from "./styled";
 
 const Header = () => {
+  const { getPartners } = useContext(GlobalStateContext);
   const [form, onChange, clear] = useForm({
     firstname: "",
     lastname: "",
     participation: "",
   });
-
+  
   const [error, setError] = useState(false);
 
   const onSubmit = (e) => {
     e.preventDefault();
-    createPartner(form, clear, setError);
+    createPartner(form, clear, setError, getPartners);
   };
 
-  const showError = () => {
-    if (error) {
-      return <div>Erro</div>;
-    }
-  };
-  
   return (
     <form onSubmit={onSubmit}>
       <Main>
@@ -32,6 +28,7 @@ const Header = () => {
           name="firstname"
           value={form.firstname}
           onChange={onChange}
+          required
         />
         <Input
           placeholder="Last name"
@@ -39,6 +36,7 @@ const Header = () => {
           name="lastname"
           value={form.lastname}
           onChange={onChange}
+          required
         />
         <Input
           placeholder="Participation"
@@ -46,10 +44,11 @@ const Header = () => {
           name="participation"
           value={form.participation}
           onChange={onChange}
+          step="any"
+          required
         />
         <Button type="submit">SEND</Button>
       </Main>
-      {showError()}
     </form>
   );
 };
--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -1,8 +1,21 @@
+import Header from "../../components/Header/Header";
+import { Button, Container, Img, Main, Text } from "./styled";
+import error from "../../assets/error.png";
+import { useHistory } from "react-router";
+import { goToHome } from "../../routes/coordinator";
+
 const Error = () => {
+  const history = useHistory()
+
   return (
-    <div>
-      <h1>A errada</h1>
-    </div>
+    <Main>
+      <Header />
+      <Container>
+        <Img src={error} alt="Error" />
+        <Text>We couldn't find this page :(</Text>
+        <Button onClick={() => goToHome(history)}>Voltar</Button>
+      </Container>
+    </Main>
   );
 };
 
